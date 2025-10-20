9921610af2a4044a7041e126f94e7008e3517dae
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-20 17:50:27 +0200

    Update provider.tf

--- Cloud Orchestration/terraform/provider.tf
+++ Cloud Orchestration/terraform/provider.tf
@@ -2,3 +2,10 @@ provider "aws" {
   region  = "eu-west-1"
   profile = "default"
 }
+
+provider "google" {
+  credentials = file("${path.root}/gcp-service-account.json")
+  project     = "cloudshirt-project"
+  region      = "europe-west4"
+  zone        = "europe-west4-a"
+}
