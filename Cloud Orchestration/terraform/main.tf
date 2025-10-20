4430d8301b42c615fc5250e8ad26eb49092ec057
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-20 17:32:20 +0200

    Update main.tf

--- Cloud Orchestration/terraform/main.tf
+++ Cloud Orchestration/terraform/main.tf
@@ -8,4 +8,14 @@ module "rds" {
   depends_on  = [module.base]
 }
 
+module "efs" {
+  source     = "./modules/aws/efs_stack"
+  depends_on = [module.base]
+}
+
+module "elk" {
+  source     = "./modules/aws/elk_stack"
+  depends_on = [module.buildserver]
+}
+
 
