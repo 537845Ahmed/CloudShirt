a1c5afb8c02791f468543db26232738f247ccbe6
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-20 17:10:31 +0200

    Update main.tf

--- Cloud Orchestration/terraform/main.tf
+++ Cloud Orchestration/terraform/main.tf
@@ -1,1 +1,39 @@
+module "base" {
+  source = "./modules/aws/base_stack"
+}
+
+module "rds" {
+  source      = "./modules/aws/rds_stack"
+  db_password = var.db_password
+  depends_on  = [module.base]
+}
+
+module "efs" {
+  source     = "./modules/aws/efs_stack"
+  depends_on = [module.base]
+}
+
+module "ecr" {
+  source = "./modules/aws/ecr_stack"
+}
+
+module "buildserver" {
+  source     = "./modules/aws/buildserver_stack"
+  depends_on = [module.base]
+}
+
+module "loadbalancer" {
+  source     = "./modules/aws/loadbalancer_stack"
+  depends_on = [module.buildserver]
+}
+
+module "elk" {
+  source     = "./modules/aws/elk_stack"
+  depends_on = [module.buildserver]
+}
+
+module "ec2docker" {
+  source     = "./modules/aws/ec2docker_stack"
+  depends_on = [module.base]
+}
 
