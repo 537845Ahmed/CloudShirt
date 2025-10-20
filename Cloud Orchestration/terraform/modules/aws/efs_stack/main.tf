24671bb12fc2956f1eb23e55088de39ae28d89d7
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-20 17:25:31 +0200

    Create main.tf

---
+++ Cloud Orchestration/terraform/modules/aws/efs_stack/main.tf
@@ -0,0 +1,13 @@
+resource "aws_cloudformation_stack" "efs" {
+  name          = "cloudshirt-efs"
+  template_body = file("${path.root}/templates/efs(2).yml")
+
+  tags = {
+    Project      = "CloudShirt"
+    Environment  = "prod"
+  }
+}
+
+output "efs_stack_id" {
+  value = aws_cloudformation_stack.efs.id
+}
