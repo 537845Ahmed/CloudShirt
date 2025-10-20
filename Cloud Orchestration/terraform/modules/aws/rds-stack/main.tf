a2c5a68ac85a014f1df7a6ac17a81efe4b1c05f3
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-20 17:20:16 +0200

    Update main.tf

--- Cloud Orchestration/terraform/modules/aws/rds-stack/main.tf
+++ Cloud Orchestration/terraform/modules/aws/rds-stack/main.tf
@@ -1,3 +1,24 @@
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+resource "aws_cloudformation_stack" "rds" {
+  name          = "cloudshirt-rds"
+  template_body = file("${path.root}/templates/rds(3).yml")
+
+  parameters = {
+    DBName     = "cloudshirt"
+    DBUser     = "csadmin"
+    DBPassword = var.db_password
+  }
+
+  tags = {
+    Project      = "CloudShirt"
+    Environment  = "prod"
+  }
+}
+
 output "rds_stack_id" {
   value = aws_cloudformation_stack.rds.id
 }
