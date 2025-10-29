6ca0f22b72f9ae82294fed3cfbe7d5199fe9fe8f
537845Ahmed <118907976+537845Ahmed@users.noreply.github.com>
2025-10-29 22:02:33 +0100

    Add files via upload

--- Cloud Orchestration/terraform/modules/gcp/artifact_registry/main.tf
+++ Cloud Orchestration/terraform/modules/gcp/artifact_registry/main.tf
@@ -1,6 +1,7 @@
 resource "google_artifact_registry_repository" "cloudshirt_repo" {
-  location      = "europe-west4"
-  repository_id = "cloudshirt-repo"
+  project       = var.project_id
+  location      = var.gcp_region
+  repository_id = var.gcp_repo_name
   description   = "Artifact Registry for CloudShirt images"
   format        = "DOCKER"
 }
